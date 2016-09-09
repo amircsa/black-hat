88838c6cde39a6d5691ccefa29bc509bca285b41
amiroo126m <seyeda.126@gmail.com>
2016-09-09 20:06:38 +0430

    Update hello.lua

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -1,15 +1,15 @@
 do
 
 function run(msg, matches)
-  return "Hello, " .. matches[1]
+  return "سلام, " .. matches[1]
 end
 
 return {
   description = "Says hello to someone", 
-  usage = "say hello to [name]",
+  usage = "say سلام to [name]",
   patterns = {
-    "^say hello to (.*)$",
-    "^Say hello to (.*)$"
+    "^say سلام to (.*)$",
+    "^Say سلام to (.*)$"
   }, 
   run = run 
 }
